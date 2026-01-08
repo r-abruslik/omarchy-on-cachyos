f60db59cef65000e6eebbe8f91c909293b3431f9
r-abruslik <55549730+r-abruslik@users.noreply.github.com>
2026-01-08 05:12:53 -0500

    Fix sed commands in install-omarchy-on-cachyos.sh

--- bin/install-omarchy-on-cachyos.sh
+++ bin/install-omarchy-on-cachyos.sh
@@ -70,10 +70,11 @@ echo ">> Applying CachyOS compatibility patches..."
 [[ ! -f "install.sh" ]] && die "install.sh not found. Make sure you are inside the omarchy folder!"
 
 if [[ -f "bin/omarchy-update-restart" ]]; then
-    sed -i "s/ | sed 's\/-arch\/\\\\.arch\/'//'" bin/omarchy-update-restart
-    sed -i "s/'{print \$2}'/'{print \$2 \" - \" \$1}' | sed 's\/-linux\/\//'/" bin/omarchy-update-restart
+    sed -i "s# | sed 's/-arch/\\\\.arch/'##" bin/omarchy-update-restart
+    sed -i "s#'{print \$2}'#'{print \$2 \" - \" \$1}' | sed 's/-linux//'#" bin/omarchy-update-restart
     sed -i "/linux-cachyos/ ! s/pacman -Q linux/pacman -Q linux-cachyos/" bin/omarchy-update-restart
 fi
+
 
 
 if [[ -f "install/omarchy-base.packages" ]]; then
